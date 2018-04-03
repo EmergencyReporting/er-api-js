fe87d949ea415f12bb4a8cef61238f508dfc2690
Gabe Martin <gabema@gmail.com>
2018-04-03 06:53:45 -0700

    adding apparatus calls

--- apiv2/apparatus.js
+++ apiv2/apparatus.js
@@ -5,7 +5,7 @@ const {getAPIEnv} = require('../env');
 // defaulting timeout to 30 seconds
 axios.defaults.timeout = 30000;
 
-const getApparatuses = params => getAccessToken().then(accessToken => axios({
+const getAllApparatus = params => getAccessToken().then(accessToken => axios({
     url: `${getAPIEnv().url}/V2/apparatus`,
     method: 'get',
     headers: {
@@ -23,7 +23,61 @@ const getApparatus = (departmentApparatusID, params) => getAccessToken().then(ac
     params
 }).then(response => response.data));
 
-const getApparatusMaintenance = (departmentApparatusID, params) => getAccessToken().then(accessToken => axios({
+const getAllApparatusCompartments = params => getAccessToken().then(accessToken => axios({
+    url: `${getAPIEnv().url}/V2/apparatus/compartments`,
+    method: 'get',
+    headers: {
+        Authorization: accessToken
+    },
+    params
+}).then(response => response.data));
+
+const getApparatusCompartment = (compartmentID, params) => getAccessToken().then(accessToken => axios({
+    url: `${getAPIEnv().url}/V2/apparatus/compartments/${compartmentID}`,
+    method: 'get',
+    headers: {
+        Authorization: accessToken
+    },
+    params
+}).then(response => response.data));
+
+const getAllApparatusCrews = params => getAccessToken().then(accessToken => axios({
+    url: `${getAPIEnv().url}/V2/apparatus/crews`,
+    method: 'get',
+    headers: {
+        Authorization: accessToken
+    },
+    params
+}).then(response => response.data));
+
+const getApparatusCrews = (departmentApparatusID, params) => getAccessToken().then(accessToken => axios({
+    url: `${getAPIEnv().url}/V2/apparatus/${departmentApparatusID}/crews`,
+    method: 'get',
+    headers: {
+        Authorization: accessToken
+    },
+    params
+}).then(response => response.data));
+
+const getApparatusCrew = (departmentApparatusID, crewID, params) => getAccessToken().then(accessToken => axios({
+    url: `${getAPIEnv().url}/V2/apparatus/${departmentApparatusID}/crews/${crewID}`,
+    method: 'get',
+    headers: {
+        Authorization: accessToken
+    },
+    params
+}).then(response => response.data));
+
+const getAllApparatusMaintenances = params => getAccessToken().then(accessToken => axios({
+    url: `${getAPIEnv().url}/V2/apparatus/maintenance`,
+    method: 'get',
+    headers: {
+        Authorization: accessToken
+    },
+    params
+}).then(response => response.data));
+
+const getApparatusMaintenances = (departmentApparatusID, params) => getAccessToken().then(accessToken => axios({
     url: `${getAPIEnv().url}/V2/apparatus/${departmentApparatusID}/maintenance`,
     method: 'get',
     headers: {
@@ -32,8 +86,24 @@ const getApparatusMaintenance = (departmentApparatusID, params) => getAccessToke
     params
 }).then(response => response.data));
 
+const getApparatusMaintenance = (departmentApparatusID, maintenanceID, params) => getAccessToken().then(accessToken => axios({
+    url: `${getAPIEnv().url}/V2/apparatus/${departmentApparatusID}/maintenance/${maintenanceID}`,
+    method: 'get',
+    headers: {
+        Authorization: accessToken
+    },
+    params
+}).then(response => response.data));
+
 module.exports = {
-    getApparatuses,
+    getAllApparatus,
     getApparatus,
+    getAllApparatusCompartments,
+    getApparatusCompartment,
+    getAllApparatusCrews,
+    getApparatusCrews,
+    getApparatusCrew,
+    getAllApparatusMaintenances,
+    getApparatusMaintenances,
     getApparatusMaintenance
 };
